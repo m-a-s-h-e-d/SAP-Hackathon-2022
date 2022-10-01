a86d162fb6f786346afbc9698eeb831ac73fcc38
Jacky Tsoi <jackytsoi2015@outlook.com>
2022-10-01 11:56:05 -0700

    Add login endpoint

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,26 @@
-const express = require("express");
+import express from "express";
+import cors from "cors";
 
 const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.post("/login", (req, res) => {
+  res.send("Random Login Token Hash");
+  console.log(req.body);
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
