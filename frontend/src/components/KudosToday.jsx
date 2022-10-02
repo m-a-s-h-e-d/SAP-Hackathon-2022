86fe334bdd73fb1c5bcf34814eb756603212de91
Matthew <Matirix@live.com>
2022-10-01 20:04:57 -0700

    Toched up on Kudo'sToday

--- frontend/src/components/KudosToday.jsx
+++ frontend/src/components/KudosToday.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 function KudosToday() {
     // Messages Board
+    
 
     //Send one back
     //Add to Scrapbook
@@ -9,47 +10,65 @@ function KudosToday() {
         {
             id: 1,
             name: "Jacky",
-            message: "Hey, Thanks for helping me set up shop"
+            message: "Hey, Thanks for helping me set up shop",
+            pic: "https://picsum.photos/seed/picsum/40/40"
 
         },
         {
             id: 2,
             name: "Dustin",
-            message: "That was really well thought out!"
+            message: "That was really well thought out!",
+            pic: "https://picsum.photos/seed/picsum/40/40"
+
+            
         },
         {
             id: 3,
             name: "Matthew",
-            message: "Keep up the good work!"
+            message: "Keep up the good work!",
+            pic: "https://picsum.photos/seed/picsum/40/40"
+
         }
     ]
 
 
 
     return (
-        <div className=''>
+        <div className='my-3'>
 
-            {messages.map(({ id, name, message }) => (
+            {messages.map(({ id, name, message, pic }) => (
                 //Id of each one [needed to map]
-                <div key={id} className="border rounded-sm my-2">
-                    {/* Name and Buttons */}
-                    <div className="name">
-                        {name}
+                <div key={id} className="shadow-md border rounded-lg  py-3 px-3">
+                    <div className='flex titleAndButtons justify-between'>
+                    <img src={pic} alt="" />
+
+                        {/* Name */}
+                        <text className="name">
+                            
+                            {name}
+                        </text>
                         {/* Buttons */}
+                    
+                        <div className='ButtonsToRight'>
                         <span>
-                            <button className='border'>
-                                Add to scrapbook
-                                {/* Add to Scrapbook */}
-                            </button>
-                        </span>
-                        <span>
-                            <button className='border'>
-                                Send Kudos back
+                                {/* Add to ScrapBook */}
+                                <button className='mx-auto'>
+                                <object className="mx-auto my-auto" type="image/svg+xml" data='../images/reply.svg' >
+                            </object>
+                                </button>
+                            </span>
+                            <span>
                                 {/* SendKudos */}
-                            </button>
+                                <button className='my-auto'>
+                                <object className="mx-auto my-auto" type="image/svg+xml" data='../images/favorite_border.svg' >
+                                </object>
+                                </button>
                         </span>
+                        </div>
 
                     </div>
+
+
 
 
                     {/* message */}
